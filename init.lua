dc85f20b8f54ec81cc9c3004d90cc3399a386915
Hee Zhan Zhynn <zhanzhynn@gmail.com>
2025-06-07 18:30:23 +0800

    fix yank to system clipboard and add fuzzy lsp matcher

--- init.lua
+++ init.lua
@@ -199,6 +199,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- xclip needed for linux
+vim.opt.clipboard = 'unnamedplus'
+
 -- [[ Custom Keymaps ]]
 vim.keymap.set('n', '<leader>p', vim.cmd.Ex, { desc = 'File explorer' })
 
